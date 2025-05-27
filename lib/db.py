de284ec3dd3e81bbfd24d964ea8f205f60dab3e2
Njuguna <joshuangich2@gmail.com>
2025-05-28 01:19:32 +0300

    Initial Commit

---
+++ lib/db.py
@@ -0,0 +1,11 @@
+# db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Use SQLite for simplicity
+DATABASE_URL = "sqlite:///cereal.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
---
+++ lib/models.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from db import Base
+
+
+
+class Supplier(Base):
+    __tablename__ = 'supplier'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    cereals = relationship("Cereal", back_populates="supplier")
+
+
+class Customer(Base):
+    __tablename__ = 'customer'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+
+class Order(Base):
+    __tablename__ = 'order'
+
+    id = Column(Integer, primary_key=True)
+    cereal_id = Column(Integer, ForeignKey('cereal.id'))
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    status = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    cereal_ordered = relationship("Cereal", back_populates="order")
+    customer = relationship("Customer", back_populates="orders")
+
+
+class Cereal(Base):
+    __tablename__ = 'cereal'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    order_id = Column(Integer, ForeignKey('order.id'))
+    supplier_id = Column(Integer, ForeignKey('supplier.id'))
+
+    order = relationship("Order", back_populates="cereal_ordered")
+    supplier = relationship("Supplier", back_populates="cereals")
